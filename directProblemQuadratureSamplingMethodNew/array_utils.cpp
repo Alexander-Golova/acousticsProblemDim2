41192bdd4353bef5abe85bdffc174713ba5216ca
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2017-06-30 19:09:35 +0300

    В отдельный модуль выделен счет основных массивов

---
+++ directProblemQuadratureSamplingMethodNew/array_utils.cpp
@@ -0,0 +1,52 @@
+#include "stdafx.h"
+
+#include "taskData.h"
+#include "array_utils.h"
+
+using namespace std;
+
+void WriteBasicArraysFile(vector<vector<vector<vector<complex<double>>>>> & a,
+	vector<vector<vector<complex<double>>>> & overline_a, vector<vector<complex<double>>> & b)
+{
+	ofstream f_a("matrix_a.txt");
+	f_a << fixed << setprecision(6);
+	for (size_t i = 0; i <= NUMBER_PARTITION_POINT; ++i)
+	{
+		for (size_t j = 0; j <= NUMBER_PARTITION_POINT; ++j)
+		{
+			for (size_t p = 0; p < NUMBER_PARTITION_POINT; ++p)
+			{
+				for (size_t q = 0; q < NUMBER_PARTITION_POINT; ++q)
+				{
+					f_a << a[i][j][p][q] << " ";
+				}
+			}
+		}
+	}
+	f_a.close();
+
+	ofstream f_overline_a("matrix_overline_a.txt");
+	f_overline_a << fixed << setprecision(6);
+	for (size_t j = 0; j <= NUMBER_PARTITION_POINT; ++j)
+	{
+		for (size_t p = 0; p < NUMBER_PARTITION_POINT; ++p)
+		{
+			for (size_t q = 0; q < NUMBER_PARTITION_POINT; ++q)
+			{
+				f_overline_a << overline_a[j][p][q] << " ";
+			}
+		}
+	}
+	f_overline_a.close();
+
+	ofstream f_b("matrix_b.txt");
+	f_b << fixed << setprecision(6);
+	for (size_t i = 0; i <= NUMBER_PARTITION_POINT; ++i)
+	{
+		for (size_t j = 0; j <= NUMBER_PARTITION_POINT; ++j)
+		{
+			f_b << b[i][j] << " ";
+		}
+	}
+	f_b.close();
+}
