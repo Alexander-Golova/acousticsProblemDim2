bf4fd546c751ebbb6e5cf7e5f506062c771bc1ee
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2017-08-04 11:22:23 +0300

    Рефакторинг

--- directProblemQuadratureSamplingMethodNew/Sources.h
+++ directProblemQuadratureSamplingMethodNew/Sources.h
@@ -11,7 +11,7 @@ struct Source
 	const size_t numberSource = 5;
 	// координаты источников на двухмерной плоскости
 	const std::vector<Point> node = {
-		{ -1.0, 0.0 },{ -1.0, 2.5 },{ -1.0, 5.0 },{ -1.0, 7.5 },{ -1.0, 10.0 } };
+		{ -0.1, 0.0 }, { -0.1, 2.0 }, { -0.1, 4.0 }, { -0.1, 6.0 }, { -0.1, 8.0 }, { -0.1, 10.0 } };
 	std::complex<double> Function(const Point source, const double x, const double y) const;
 };
 
