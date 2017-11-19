cd36998edfabb5b36bfed867a33e079f3646a852
alexander kozlov <al.iv.kozlov@yandex.ru>
2017-11-19 11:48:33 +0300

    Удалил 2D задачу со сплайнами

--- DirectProblemWithSplineSampling/exact_solution.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void GetExactSolution(std::array<std::array<std::complex<double>, SPLITTING + 1>, SPLITTING + 1> & xi);
-
-void WriteSolutionFile(std::array<std::array<std::complex<double>, SPLITTING + 1>, SPLITTING + 1> & xi);
--- directProblemQuadratureSamplingMethodNew/exact_solution.cpp
+++ directProblemQuadratureSamplingMethodNew/exact_solution.cpp
@@ -6,11 +6,12 @@ using namespace std;
 
 void GetExactSolution(vector<vector<double>> & xi)
 {
+	double sigma = 4;
 	for (size_t i = 0; i <= NUMBER_PARTITION_POINT; ++i)
 	{
 		for (size_t j = 0; j <= NUMBER_PARTITION_POINT; ++j)
 		{
-			xi[i][j] = exp(-(i * h - 0.6) * (i * h - 0.6) - (j * h - 0.6) * (j * h - 0.6));
+			xi[i][j] = exp(-((i * h - 0.6) * (i * h - 0.6) + (j * h - 0.6) * (j * h - 0.6)) / sigma);
 		}
 	}
 }
