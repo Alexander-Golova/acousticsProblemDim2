cf8a72b39c944e1e02bc2abe5a10893145e7f1e5
alexander kozlov <al.iv.kozlov@yandex.ru>
2017-11-27 09:59:22 +0300

    Убраны лишние дирректории

--- directProblemQuadratureSamplingMethodNew/array_utils.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void WriteBasicArraysFile(std::vector<std::vector<std::vector<std::vector<std::complex<double>>>>> & a,
-	std::vector<std::vector<std::vector<std::complex<double>>>> & overline_a,
-	std::vector<std::vector<std::complex<double>>> & b);
-
--- directProblemQuadratureSamplingMethodNew/basicArrays.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "taskData.h"
-#include <vector>
-#include <complex>
-
-void GetBasicArrays(std::vector<std::vector<std::vector<std::vector<std::complex<double>>>>> & a,
-     std::vector<std::vector<std::vector<std::complex<double>>>> & overline_a,
-     std::vector<std::vector<std::complex<double>>> & b);
-
