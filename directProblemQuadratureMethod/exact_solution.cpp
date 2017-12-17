07dd1d4031dd847cdfebc6a2fb6c672ea9653503
alexander kozlov <al.iv.kozlov@yandex.ru>
2017-12-17 12:15:32 +0300

    Добавлены автотесты

--- directProblemQuadratureMethod/exact_solution.cpp
+++ directProblemQuadratureMethod/exact_solution.cpp
@@ -6,14 +6,14 @@ using namespace std;
 
 void GetExactSolution(vector<vector<double>> & xi)
 {
-	double sigma = 16;
+	double sigma = 25;
 	for (size_t i = 0; i <= NUMBER_PARTITION_POINT; ++i)
 	{
 		for (size_t j = 0; j <= NUMBER_PARTITION_POINT; ++j)
 		{
-			//xi[i][j] = exp(-((i * h - 0.6) * (i * h - 0.6) + (j * h - 0.6) * (j * h - 0.6)) * sigma);
-			xi[i][j] = 0.8 * exp(-((i * h - 0.8) * (i * h - 0.8) + (j * h - 0.8) * (j * h - 0.8)) * sigma) +
-				0.2 * exp(-((i * h - 0.2) * (i * h - 0.2) + (j * h - 0.2) * (j * h - 0.2)) * sigma);
+			xi[i][j] = exp(-((i * h - 0.6) * (i * h - 0.6) + (j * h - 0.6) * (j * h - 0.6)) * sigma);
+			//xi[i][j] = 0.8 * exp(-((i * h - 0.8) * (i * h - 0.8) + (j * h - 0.8) * (j * h - 0.8)) * sigma) +
+				//0.2 * exp(-((i * h - 0.2) * (i * h - 0.2) + (j * h - 0.2) * (j * h - 0.2)) * sigma);
 		}
 	}
 }
