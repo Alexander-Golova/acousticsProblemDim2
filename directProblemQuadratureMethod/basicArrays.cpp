a9f36776d15e29cc114251144fca83d11d66a9d6
alexander kozlov <al.iv.kozlov@yandex.ru>
2017-12-30 00:18:44 +0300

    Основные массивы разделены на действительные и мнимые

--- directProblemQuadratureMethod/basicArrays.cpp
+++ directProblemQuadratureMethod/basicArrays.cpp
@@ -5,12 +5,14 @@
 
 using namespace std;
 
-void GetBasicArrays(vector<vector<vector<vector<complex<float>>>>> & a,
-     vector<vector<vector<complex<float>>>> & overline_a, vector<vector<complex<float>>> & b)
+void GetBasicArrays(vector<vector<vector<vector<float>>>> & a,
+	vector<vector<vector<vector<float>>>> & b,
+	vector<vector<float>> & c,
+	vector<vector<vector<float>>> & overline_a,
+	vector<vector<vector<float>>> & overline_b)
 {
 	// счет индексов метода квадратур
 	vector<float> index(NUMBER_PARTITION_POINT + 1);
-
 	for (size_t i = 1; i < NUMBER_PARTITION_POINT; ++i)
 	{
 		if (i % 2 != 0)
@@ -26,6 +28,7 @@ void GetBasicArrays(vector<vector<vector<vector<complex<float>>>>> & a,
 	index[NUMBER_PARTITION_POINT] = 1.0f / 3;
 
 	// нахождение массива a
+	float dist;
 	for (size_t i = 0; i <= NUMBER_PARTITION_POINT; ++i)
 	{
 		for (size_t j = 0; j <= NUMBER_PARTITION_POINT; ++j)
@@ -34,27 +37,16 @@ void GetBasicArrays(vector<vector<vector<vector<complex<float>>>>> & a,
 			{
 				for (size_t q = 0; q < NUMBER_PARTITION_POINT; ++q)
 				{
-					if ((i != p) || (q != j))
+					if ((i != p) && (q != j))
 					{
-						a[i][j][p][q] = index[p] * index[q];
-						a[i][j][p][q] = a[i][j][p][q] * G(i * h, j * h, p * h, q * h);
-						a[i][j][p][q] = a[i][j][p][q] * h * h;
+						dist = step * sqrt(pow(static_cast<float>(i - p), 2) + pow(static_cast<float>(j - q), 2));
+						a[i][j][p][q] = index[p] * index[q] * 0.25f * N_0(dist) * pow(OMEGA, 2) * pow(step, 2);
+					}
+					else
+					{
+						a[i][j][p][q] = 0.0f;
 					}
 				}
-			}
-		}
-	}
-
-	// нахождение массива overline_a
-	for (size_t j = 0; j <= NUMBER_PARTITION_POINT; ++j)
-	{
-		for (size_t p = 0; p < NUMBER_PARTITION_POINT; ++p)
-		{
-			for (size_t q = 0; q < NUMBER_PARTITION_POINT; ++q)
-			{
-				overline_a[j][p][q] = index[p] * index[q];
-				overline_a[j][p][q] = overline_a[j][p][q] * G(receiver, j * h, p * h, q * h);
-				overline_a[j][p][q] = overline_a[j][p][q] * h * h;
 			}
 		}
 	}
@@ -68,13 +60,61 @@ void GetBasicArrays(vector<vector<vector<vector<complex<float>>>>> & a,
 			{
 				for (size_t q = 0; q < NUMBER_PARTITION_POINT; ++q)
 				{
-					if (i != p)
-					{
-						b[i][j] += G(i * h, j * h, p * h, q * h);
-					}
+					dist = step * sqrt(pow(static_cast<float>(i - p), 2) + pow(static_cast<float>(j - q), 2));
+					b[i][j][p][q] = index[p] * index[q] * (-0.25f) * J_0(dist) * pow(OMEGA, 2) * pow(step, 2);
 				}
-				b[i][j] *= h * h;
 			}
 		}
 	}
+
+	// нахождение массива c
+	for (size_t i = 0; i <= NUMBER_PARTITION_POINT; ++i)
+	{
+		for (size_t j = 0; j <= NUMBER_PARTITION_POINT; ++j)
+		{
+			c[i][j] = 0.0f;
+			for (size_t p = 0; p < NUMBER_PARTITION_POINT; ++p)
+			{
+				for (size_t q = 0; q < NUMBER_PARTITION_POINT; ++q)
+				{
+					if ((i != p) && (q != j))
+					{
+						dist = step * sqrt(pow(static_cast<float>(i - p), 2) + pow(static_cast<float>(j - q), 2));
+						c[i][j] += index[p] * index[q] * 0.25f * N_0(dist) * pow(OMEGA, 2) * pow(step, 2);
+					}
+					else
+					{
+						a[i][j][p][q] = 0.0f;
+					}
+				}
+			}
+		}
+	}
+
+	// нахождение массива overline_a
+	for (size_t j = 0; j <= NUMBER_PARTITION_POINT; ++j)
+	{
+		for (size_t p = 0; p < NUMBER_PARTITION_POINT; ++p)
+		{
+			for (size_t q = 0; q < NUMBER_PARTITION_POINT; ++q)
+			{
+				dist = sqrt(pow(receiver - static_cast<float>(p) * step, 2) + pow(static_cast<float>(j - q) * step, 2));
+				overline_a[j][p][q] = index[p] * index[q] * 0.25f * N_0(dist) * pow(OMEGA, 2) * pow(step, 2);
+			}
+		}
+	}
+
+	// нахождение массива overline_b
+	for (size_t j = 0; j <= NUMBER_PARTITION_POINT; ++j)
+	{
+		for (size_t p = 0; p < NUMBER_PARTITION_POINT; ++p)
+		{
+			for (size_t q = 0; q < NUMBER_PARTITION_POINT; ++q)
+			{
+				dist = sqrt(pow(receiver - static_cast<float>(p) * step, 2) + pow(static_cast<float>(j - q) * step, 2));
+				overline_b[j][p][q] = index[p] * index[q] * (-0.25f) * J_0(dist) * pow(OMEGA, 2) * pow(step, 2);
+			}
+		}
+	}
+
 }
--- directProblemQuadratureMethod/basicArrays.h
+++ directProblemQuadratureMethod/basicArrays.h
@@ -3,7 +3,9 @@
 #include <vector>
 #include <complex>
 
-void GetBasicArrays(std::vector<std::vector<std::vector<std::vector<std::complex<float>>>>> & a,
-     std::vector<std::vector<std::vector<std::complex<float>>>> & overline_a,
-     std::vector<std::vector<std::complex<float>>> & b);
+void GetBasicArrays(std::vector<std::vector<std::vector<std::vector<float>>>> & a,
+	std::vector<std::vector<std::vector<std::vector<float>>>> & b,
+	std::vector<std::vector<float>> & c,
+	std::vector<std::vector<std::vector<float>>> & overline_a,
+	std::vector<std::vector<std::vector<float>>> & overline_b);
 
