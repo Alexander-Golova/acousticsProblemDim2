762eed5c52016d0f35edb13cf4a44210dc6d8f6b
alexander kozlov <al.iv.kozlov@yandex.ru>
2017-12-30 00:26:16 +0300

    Изменены методы загрузки массивов

--- directProblemQuadratureMethod/array_utils.cpp
+++ directProblemQuadratureMethod/array_utils.cpp
@@ -5,8 +5,9 @@
 
 using namespace std;
 
-void WriteBasicArraysFile(vector<vector<vector<vector<complex<float>>>>> & a,
-	vector<vector<vector<complex<float>>>> & overline_a, vector<vector<complex<float>>> & b)
+void WriteBasicArraysFile(vector<vector<vector<vector<float>>>> & a,
+	vector<vector<vector<vector<float>>>> & b, vector<vector<float>> & c,
+	vector<vector<vector<float>>> & overline_a, vector<vector<vector<float>>> & overline_b)
 {
 	ofstream f_a("matrix_a.txt");
 	f_a << fixed << setprecision(6);
@@ -25,6 +26,34 @@ void WriteBasicArraysFile(vector<vector<vector<vector<complex<float>>>>> & a,
 	}
 	f_a.close();
 
+	ofstream f_b("matrix_b.txt");
+	f_b << fixed << setprecision(6);
+	for (size_t i = 0; i <= NUMBER_PARTITION_POINT; ++i)
+	{
+		for (size_t j = 0; j <= NUMBER_PARTITION_POINT; ++j)
+		{
+			for (size_t p = 0; p < NUMBER_PARTITION_POINT; ++p)
+			{
+				for (size_t q = 0; q < NUMBER_PARTITION_POINT; ++q)
+				{
+					f_b << b[i][j][p][q] << " ";
+				}
+			}
+		}
+	}
+	f_b.close();
+
+	ofstream f_c("matrix_c.txt");
+	f_c << fixed << setprecision(6);
+	for (size_t i = 0; i <= NUMBER_PARTITION_POINT; ++i)
+	{
+		for (size_t j = 0; j <= NUMBER_PARTITION_POINT; ++j)
+		{
+			f_c << c[i][j] << " ";
+		}
+	}
+	f_c.close();
+
 	ofstream f_overline_a("matrix_overline_a.txt");
 	f_overline_a << fixed << setprecision(6);
 	for (size_t j = 0; j <= NUMBER_PARTITION_POINT; ++j)
@@ -39,14 +68,17 @@ void WriteBasicArraysFile(vector<vector<vector<vector<complex<float>>>>> & a,
 	}
 	f_overline_a.close();
 
-	ofstream f_b("matrix_b.txt");
-	f_b << fixed << setprecision(6);
-	for (size_t i = 0; i <= NUMBER_PARTITION_POINT; ++i)
+	ofstream f_overline_b("matrix_overline_b.txt");
+	f_overline_b << fixed << setprecision(6);
+	for (size_t j = 0; j <= NUMBER_PARTITION_POINT; ++j)
 	{
-		for (size_t j = 0; j <= NUMBER_PARTITION_POINT; ++j)
+		for (size_t p = 0; p < NUMBER_PARTITION_POINT; ++p)
 		{
-			f_b << b[i][j] << " ";
+			for (size_t q = 0; q < NUMBER_PARTITION_POINT; ++q)
+			{
+				f_overline_b << overline_b[j][p][q] << " ";
+			}
 		}
 	}
-	f_b.close();
+	f_overline_b.close();
 }
--- directProblemQuadratureMethod/array_utils.h
+++ directProblemQuadratureMethod/array_utils.h
@@ -1,6 +1,7 @@
 #pragma once
 
-void WriteBasicArraysFile(std::vector<std::vector<std::vector<std::vector<std::complex<float>>>>> & a,
-	std::vector<std::vector<std::vector<std::complex<float>>>> & overline_a,
-	std::vector<std::vector<std::complex<float>>> & b);
-
+void WriteBasicArraysFile(std::vector<std::vector<std::vector<std::vector<float>>>> & a,
+	std::vector<std::vector<std::vector<std::vector<float>>>> & b,
+	std::vector<std::vector<float>> & c,
+	std::vector<std::vector<std::vector<float>>> & overline_a,
+	std::vector<std::vector<std::vector<float>>> & overline_b);
